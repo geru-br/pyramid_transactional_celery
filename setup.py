bfe65743bc9f6b3a5861a9585fddbfdecb8795ee
fernando <fernando.waitman@gmail.com>
2018-10-26 17:16:10 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='pyramid_transactional_celery',
-    version='0.1.2',
+    version='0.1.3',
     description='A transaction-aware Celery job setup',
     long_description=readme + '\n\n' + history,
     author='Christopher Petrilli',
